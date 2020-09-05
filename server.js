f961b6cce40f7e8903133c316d1afd9f42c259c2
montassar.bouhjila <montassar.bouhjila@gmail.com>
2020-09-05 11:33:15 +0100

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://plomberie.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Accept, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
